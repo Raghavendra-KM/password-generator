1df2da1dca7856ad51cceca2816f1197440be307
Raghavendra K M <raghavendrakm08@gmail.com>
2023-12-26 20:03:09 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const uppercase = document.getElementById('uppercase').checked;
+    const lowercase = document.getElementById('lowercase').checked;
+    const numbers = document.getElementById('numbers').checked;
+    const symbols = document.getElementById('symbols').checked;
+    const removeDuplicates = document.getElementById('removeDuplicates').checked;
+
+    let charset = generateCharset(uppercase, lowercase, numbers, symbols);
+    let password = generateRandomPassword(length, charset);
+
+    if (removeDuplicates) {
+        password = removeDuplicateCharacters(password);
+    }
+
+    document.getElementById('password').value = password;
+}
+
+function generateCharset(uppercase, lowercase, numbers, symbols) {
+    let charset = '';
+    if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (numbers) charset += '0123456789';
+    if (symbols) charset += '!@#$%^&*()-_+=<>?';
+
+    return charset;
+}
+
+function generateRandomPassword(length, charset) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+    return password;
+}
+
+function removeDuplicateCharacters(str) {
+    return [...new Set(str)].join('');
+}
